5884dcf22b8f4ff5a4142d9ecc9cdc0116f509db
高海洋 <gaohaiyang@juzifenqi.com>
2017-03-16 15:34:46 +0800

    arrow down
    arrow down click

--- app/src/main/java/com/ghy/meizustorescrolldemo/GameDetailActivity.java
+++ app/src/main/java/com/ghy/meizustorescrolldemo/GameDetailActivity.java
@@ -2,16 +2,32 @@ package com.ghy.meizustorescrolldemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.ghy.meizustorescrolldemo.utils.StatusBarUtils;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class GameDetailActivity extends AppCompatActivity {
+
+    @Bind(R.id.iv_arrow_down)
+    ImageView mIvArrowDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_detail);
+        ButterKnife.bind(this);
         StatusBarUtils.statusBarLightMode(this);
+
+        mIvArrowDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 
     @Override
@@ -19,5 +35,12 @@ public class GameDetailActivity extends AppCompatActivity {
         super.onBackPressed();
         overridePendingTransition(R.anim.current_to_bottom, R.anim.top_to_current);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
 
 }
