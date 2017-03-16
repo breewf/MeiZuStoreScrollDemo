8c706dc6a549badc1ed453c4c2524c341ff63e13
高海洋 <gaohaiyang@juzifenqi.com>
2017-03-16 18:36:37 +0800

    上滑视差效果
    上滑视差效果

--- app/src/main/java/com/ghy/meizustorescrolldemo/GameDetailActivity.java
+++ app/src/main/java/com/ghy/meizustorescrolldemo/GameDetailActivity.java
@@ -1,19 +1,40 @@
 package com.ghy.meizustorescrolldemo;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.flyco.tablayout.SlidingTabLayout;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+import com.ghy.meizustorescrolldemo.fragment.GameFragment1;
+import com.ghy.meizustorescrolldemo.fragment.GameFragment2;
+import com.ghy.meizustorescrolldemo.fragment.GameFragment3;
+import com.ghy.meizustorescrolldemo.fragment.GameFragment4;
 import com.ghy.meizustorescrolldemo.utils.StatusBarUtils;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class GameDetailActivity extends AppCompatActivity {
+public class GameDetailActivity extends AppCompatActivity implements OnTabSelectListener {
+
+
+    private String[] mTitles = {"详情", "评论", "资讯", "论坛"};
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private GamePagerAdapter mPagerAdapter;
 
     @Bind(R.id.iv_arrow_down)
     ImageView mIvArrowDown;
+    @Bind(R.id.game_tab_layout)
+    SlidingTabLayout mGameTabLayout;
+    @Bind(R.id.game_viewpager)
+    ViewPager mGameViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +49,60 @@ public class GameDetailActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        GameFragment1 gameFragment1 = new GameFragment1();
+        GameFragment2 gameFragment2 = new GameFragment2();
+        GameFragment3 gameFragment3 = new GameFragment3();
+        GameFragment4 gameFragment4 = new GameFragment4();
+        mFragments.add(gameFragment1);
+        mFragments.add(gameFragment2);
+        mFragments.add(gameFragment3);
+        mFragments.add(gameFragment4);
+
+        //设置adapter
+        mPagerAdapter = new GamePagerAdapter(getSupportFragmentManager());
+        mGameViewPager.setAdapter(mPagerAdapter);
+        //设置关联
+        mGameTabLayout.setViewPager(mGameViewPager);
+        mGameTabLayout.setOnTabSelectListener(GameDetailActivity.this);
+
+
+    }
+
+    @Override
+    public void onTabSelect(int position) {
+
+
+    }
+
+    @Override
+    public void onTabReselect(int position) {
+
+    }
+
+    /**
+     * ViewPager适配器
+     */
+    private class GamePagerAdapter extends FragmentPagerAdapter {
+
+        public GamePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/ghy/meizustorescrolldemo/fragment/GameFragment1.java
@@ -0,0 +1,24 @@
+package com.ghy.meizustorescrolldemo.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ghy.meizustorescrolldemo.R;
+
+public class GameFragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game_fragment1, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
