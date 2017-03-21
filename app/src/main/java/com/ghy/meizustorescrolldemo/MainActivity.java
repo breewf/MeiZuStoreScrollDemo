642862b9a3777d09c9cdc62dd081a9cb1589a712
高海洋 <gaohaiyang@juzifenqi.com>
2017-03-21 10:45:51 +0800

    commit
    commit

--- app/src/main/java/com/ghy/meizustorescrolldemo/MainActivity.java
+++ app/src/main/java/com/ghy/meizustorescrolldemo/MainActivity.java
@@ -5,18 +5,21 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialog;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.ghy.meizustorescrolldemo.utils.StatusBarUtils;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NestedScrollView.OnScrollChangeListener {
+
+    @Bind(R.id.iv_arrow_down)
+    ImageView mIvArrowDown;
 
     @Bind(R.id.iv_get_music)
     ImageView mIvGetMusic;
@@ -30,25 +33,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.view_status_bar)
     View mViewStatusBar;
+    @Bind(R.id.app_scroll_layout)
+    NestedScrollView mAppScrollLayout;
+
+    private BottomSheetDialog mBottomSheetDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         ButterKnife.bind(this);
-        StatusBarUtils.statusBarLightMode(this);
+
+        mAppScrollLayout.setOnScrollChangeListener(this);//滚动监听
+
+        mIvArrowDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mBottomSheetDialog != null && mBottomSheetDialog.isShowing()) {
+                    mBottomSheetDialog.dismiss();
+                }
+            }
+        });
 
         mIvGetMusic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkBottomSheet();
+                showBottomSheet();
             }
         });
 
         mBtnGetMusic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkBottomSheet();
+                showBottomSheet();
             }
         });
 
@@ -67,8 +84,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void checkBottomSheet() {
-        BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(this);
+    private void showBottomSheet() {
+        if (mBottomSheetDialog == null) mBottomSheetDialog = new BottomSheetDialog(this);
         View view = getLayoutInflater().inflate(R.layout.bottom_sheet_layout, null);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             if (mBottomSheetDialog.getWindow() == null) return;
@@ -87,6 +104,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+        Log.i("scroll", "scrollY-->>" + scrollY);
     }
 
 }
